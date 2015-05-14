f663aaca767f9619d646460604c93f2091a1f583
Yoshiki Eguchi <yoshiki.eguchi@gmail.com>
2015-05-15 00:11:39 +0900

    added JSON post function

--- test.rb
+++ test.rb
@@ -3,9 +3,40 @@
 
 require 'socket'
 require 'bundler'
+require 'net/http'
+require 'json'
 Bundler.require
 
-#ホスト、ポートの定義
+def send_json(msg)
+    uri = URI.parse('http://127.0.0.1:3000/log_views.json')
+    user_id = 1
+    time_out = 30
+
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      #リクエストインスタンス生成
+      request = Net::HTTP::Post.new(uri.path)
+      request["user-agent"] = "Ruby/#{RUBY_VERSION} MyHttpClient"
+      request["Content-Type"] = "application/json"
+      payload = {
+        "log_view" =>{
+          "user_id"=>user_id,
+          "msg"=>msg
+        },
+        "commit"=>"Create Log view"
+      }.to_json
+      request.body = payload
+      #time out
+      http.open_timeout = time_out
+      http.read_timeout = time_out
+
+      #送信
+      response = http.request(request)
+      p "====RESULT(#{uri.host})========"
+      p "==> "+response.body
+    end
+end
+
+#Juliusホスト、ポートの定義
 host = "localhost"
 port = 10500
 
@@ -37,7 +68,8 @@ while true
         #puts buff
         wordList.each do |word|
             if buff =~ /#{word}/
-              puts "#{Time.now} : #{word}を認識しました"
+              t = Time.now.strftime("%Y-%m-%d %H:%M")
+              send_json "#{t} : #{word}を認識しました"
             end
         end
       end
