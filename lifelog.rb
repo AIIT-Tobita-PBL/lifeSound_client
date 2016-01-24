a520782af956482052d0862f74af14ff967dbd11
pingineer-jp <pingineer@gmail.com>
2016-01-24 17:28:19 +0900

    石鹸のログが合意と違う形式だったので対応

--- lifelog.rb
+++ lifelog.rb
@@ -128,7 +128,7 @@ class LifeLog
 		# 施錠音フラグが立ってない場合は以降の処理は行わない
 		return if @lock_flg == false
 
-		if "handSoap" == recog_sound
+		if "石鹸を使いました。" == recog_sound
 			@debug.print("施錠後の手洗い石鹸の使用を認識")
 
 			if @handSoap_flg == false
@@ -175,7 +175,7 @@ class LifeLog
 			type = $7
 			recog_sound = $8
 
-			# 環境音出なかった場合は抜ける
+			# 環境音でなかった場合は抜ける
 			#return if @types["lifeSound"] != type
 
 			# 施錠音のチェック
@@ -184,10 +184,18 @@ class LifeLog
 			# 施錠音のフラグあった場合のみ手洗いを確認する
 			checkUgai(type, recog_sound, ts)
 
+			return [type, recog_sound]
+		end
+
+		# 石鹸対応
+		if(log =~ /(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2}) : 石鹸を使いました。/)
+			logTime = Time.local($1, $2, $3, $4, $5, $6)
+			type = "実績"
+			recog_sound = "石鹸を使いました。"
+
 			# 施錠音のフラグあった場合のみ手洗い石鹸を確認する
 			checkHandSoap(type, recog_sound, ts)
 
-			return [type, recog_sound]
 		end
 	end
 
